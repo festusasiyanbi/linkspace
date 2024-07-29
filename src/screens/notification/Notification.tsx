9e325aac0069d3eea7b8095f66d6a4a13eb31c2f
Festus <festusasiyanbi80@gmail.com>
2024-07-29 00:03:20 -0400

    created the notification screen

---
+++ src/screens/notification/Notification.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import React from 'react';
+import CustomText from '../../utils/CustomText';
+
+const Notification = () => {
+  return (
+    <View>
+      <CustomText>Notification</CustomText>
+    </View>
+  );
+};
+
+export default Notification;
